68dc39ff338cb063b420879bd4b32335c58a7ca1
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-09-06 09:57:49 +0200

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,10 @@ import Nav from './nav/Nav'
 const Header = () => {
   return (
     <header className='flex px-3.5 py-6'>
-      <span className="text-3xl font-bold">Next Blog</span>
-      <Nav/>
+      <div className="container-center justify-between">
+        <span className="text-3xl font-bold">Next Blog</span>
+        <Nav />
+      </div>
     </header>
   )
 }
