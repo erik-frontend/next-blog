70b0983a8ecb671ca0f3ef3835dc55a9f4b2152d
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-09-17 15:01:29 +0200

    commit

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,31 +1,31 @@
+import PostCard from '@/components/postCard/PostCard'
 import React from 'react'
 
 const getData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=30")
-  if(!res.ok) {
+  if (!res.ok) {
     throw new Error("something wrong")
   }
   return res.json()
 }
 
-export default async function BlogPage () {
+export default async function BlogPage() {
 
   const posts = await getData()
 
-  console.log(posts);
-  
+  // console.log(posts);
+
 
   return (
-    <section>
-        <h1>Blog</h1>
+    <section className='max-auto container-center'>
+      <h1>Blog</h1>
+      <div className="flex gap-5 flex-wrap justify-between">
         {posts && (
-          posts.map((post, index) => (
-            <div className="" key={post.id}>
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-            </div>
+          posts.map((post, i) => (
+            <PostCard key={post.id} post={post} index={i} />
           ))
         )}
+      </div>
     </section>
   )
 }
